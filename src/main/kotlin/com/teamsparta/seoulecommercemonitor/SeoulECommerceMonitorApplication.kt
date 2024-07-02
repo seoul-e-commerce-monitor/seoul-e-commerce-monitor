b1dd829eaa7e38f4e3d08e46931cefadf6cb9c1d
1hyung <ryu1hyung@naver.com>
2024-07-02 14:47:06 +0900

    :tada: initial project

---
+++ src/main/kotlin/com/teamsparta/seoulecommercemonitor/SeoulECommerceMonitorApplication.kt
@@ -0,0 +1,11 @@
+package com.teamsparta.seoulecommercemonitor
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SeoulECommerceMonitorApplication
+
+fun main(args: Array<String>) {
+    runApplication<SeoulECommerceMonitorApplication>(*args)
+}
---
+++ src/test/kotlin/com/teamsparta/seoulecommercemonitor/SeoulECommerceMonitorApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.teamsparta.seoulecommercemonitor
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SeoulECommerceMonitorApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
