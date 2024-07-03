1d6dae758645af80d12209f32280363b5350592c
Jiho <https://github.com/HJH1111>
2024-07-03 21:03:33 +0900

    :sparkles: 커서 기반 페이지네이션 추

--- src/main/kotlin/com/teamsparta/seoulecommercemonitor/domain/store/controller/v1/StoreController.kt
+++ src/main/kotlin/com/teamsparta/seoulecommercemonitor/domain/store/controller/v1/StoreController.kt
@@ -1,10 +1,10 @@
 package com.teamsparta.seoulecommercemonitor.domain.store.controller.v1
 
 import com.teamsparta.seoulecommercemonitor.domain.store.dto.CsvResponse
-import com.teamsparta.seoulecommercemonitor.domain.store.model.v1.Store
 import com.teamsparta.seoulecommercemonitor.domain.store.service.v1.StoreService
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Slice
 import org.springframework.data.web.PageableDefault
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -19,27 +19,12 @@ class StoreController(private val storeService: StoreService) {
 
     @GetMapping("/filter")
     fun getAllStoresPage(
-        @PageableDefault(size = 10, sort = ["monitoringDate"]) pageable: Pageable,
         @PathVariable businessName: String?,
         @RequestParam(value ="overallEvaluation", required = false) overallEvaluation:String?,
         @RequestParam(value = "businessStatus", required = false) businessStatus: String?,
         @RequestParam(value = "monitoringDate", required = false) monitoringDate: String?,
-    ): ResponseEntity<Page<CsvResponse>> {
-        return ResponseEntity.ok(storeService.getAllStoresPage(pageable,businessName, overallEvaluation, businessStatus, monitoringDate))
+        @RequestParam(value = "csvId", required = false) csvId: Long?
+    ): ResponseEntity<List<CsvResponse>> {
+        return ResponseEntity.ok(storeService.getAllStoresPage(businessName, overallEvaluation, businessStatus, monitoringDate, csvId))
     }
-
-    @GetMapping
-    fun getAllStores(): List<Store> = storeService.getAllStores()
-
-    @GetMapping("{rating}")
-    fun getStoresByRating(@PathVariable rating: Int): List<Store> = storeService.getStoresByRating(rating)
-
-    @GetMapping("{status}")
-    fun getStoresByStatus(@PathVariable status: String): List<Store> = storeService.getStoresByStatus(status)
-
-    @GetMapping("/{RatingAndStatus}")
-    fun getStoresByRatingAndStatus(
-        @RequestParam rating: Int,
-        @RequestParam status: String
-    ): List<Store> = storeService.getStoresByRatingAndStatus(rating, status)
 }
--- src/main/kotlin/com/teamsparta/seoulecommercemonitor/domain/store/service/v1/StoreService.kt
+++ src/main/kotlin/com/teamsparta/seoulecommercemonitor/domain/store/service/v1/StoreService.kt
@@ -1,34 +1,16 @@
 package com.teamsparta.seoulecommercemonitor.domain.store.service.v1
 
 import com.teamsparta.seoulecommercemonitor.domain.store.dto.CsvResponse
-import com.teamsparta.seoulecommercemonitor.domain.store.model.v1.Store
 import com.teamsparta.seoulecommercemonitor.domain.store.model.v1.toCsvResponse
 import com.teamsparta.seoulecommercemonitor.domain.store.repository.v1.CsvRepository
-import com.teamsparta.seoulecommercemonitor.domain.store.repository.v1.StoreRepository
-import org.springframework.data.domain.Page
-import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 
 @Service
 class StoreService(
-    private val storeRepository: StoreRepository,
     private val csvRepository: CsvRepository
 ) {
 
-    fun getAllStoresPage(pageable: Pageable,businessName:String?, overallEvaluation: String?, businessStatus: String?,monitoringDate:String?): Page<CsvResponse> {
-        return csvRepository.findByStorePage(pageable,businessName ,overallEvaluation, businessStatus,monitoringDate).map { it.toCsvResponse() }
-    }
-
-    fun getAllStores(): List<Store> = storeRepository.findAll()
-
-    fun getStoresByRating(rating: Int): List<Store> = storeRepository.findByRating(rating)
-
-    fun getStoresByStatus(status: String): List<Store> = storeRepository.findByStatus(status)
-
-    fun getStoresByRatingAndStatus(rating: Int, status: String): List<Store> {
-        return storeRepository.findAll()
-            .filter { it.rating == rating && it.status == status }
-            .sortedByDescending { it.monitoringDate }
-            .take(10)
+    fun getAllStoresPage(businessName:String?, overallEvaluation: String?, businessStatus: String?,monitoringDate:String?, csvId: Long?): List<CsvResponse> {
+        return csvRepository.findByStorePage(businessName ,overallEvaluation, businessStatus,monitoringDate, csvId).map { it.toCsvResponse() }
     }
 }
